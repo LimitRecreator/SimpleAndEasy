1d4e4c19fa5b45df73f9a2c5e9d0fe3cbb5dfa28
6.8.4 <118214293+LimitRecreator@users.noreply.github.com>
2023-06-04 15:45:45 -0300

    Add files via upload

--- BR_AutoAnonsurf/autoanonsurf.py
+++ BR_AutoAnonsurf/autoanonsurf.py
@@ -1,24 +1,22 @@
-import os,sys,time
-
-timetonodechange = int(input("Tempo em segundos para troca IP :"))
-
-os.system("anonsurf start") 
-time.sleep(2.5)
+import os
+os.system("sudo apt update && sudo apt upgrade")
+install = input("Já possuí o Kali-Anonsurf instalado?(y/n): ")
+if install == ("n"):
+    os.system("git clone https://github.com/Und3rf10w/kali-anonsurf")
+    os.system("cd kali-anonsurf")
+    os.system("sudo bash installer.sh")
+if install == ("N"):
+    os.system("git clone https://github.com/Und3rf10w/kali-anonsurf")
+    os.system("cd kali-anonsurf")
+    os.system("sudo bash installer.sh")
+start = input("Gostaria de iniciar o kali anonsurf?(y/n): ")
+if start == ("y"):
+    os.system("sudo anonsurf start")
+elif start == ("Y"):
+    os.system("sudo anonsurf start")
+if start == ("n"):
+    print ("Certo, quando quiser iniciar o anonsurf, basta me rodar de novo!")
+elif start == ("N"):
+    print("Certo, quando quiser iniciar o anonsurf, basta me rodar de novo!")
 os.system("anonsurf status")
-time.sleep(2.5)
-os.system("anonsurf myip")
-time.sleep(2.5)
-
-i = 0
-
-def ipchange():
-   os.system("anonsurf myip")
-   time.sleep(2.5)  
-   os.system("anonsurf change")
-   time.sleep(2.5)  
-   os.system("anonsurf myip") 
-   time.sleep(2.5) 
-
-while i == 0:
-   time.sleep(timetonodechange)
-   ipchange()
+print("Para desligar seu anonsurf dê o comando 'sudo anonsurf stop'")
