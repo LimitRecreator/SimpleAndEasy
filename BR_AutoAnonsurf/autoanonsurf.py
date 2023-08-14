30a80b9c4695a5e953cbbce69669c65315af8f20
D0N4T3LL0 <118214293+LimitRecreator@users.noreply.github.com>
2023-08-14 12:56:25 -0300

    Update autoanonsurf.py

--- BR_AutoAnonsurf/autoanonsurf.py
+++ BR_AutoAnonsurf/autoanonsurf.py
@@ -4,11 +4,11 @@ install = input("Já possuí o Kali-Anonsurf instalado?(y/n): ")
 if install == ("n"):
     os.system("git clone https://github.com/Und3rf10w/kali-anonsurf")
     os.system("cd kali-anonsurf")
-    os.system("sudo ./installer.sh")
+    os.system("sudo bash installer.sh")
 if install == ("N"):
     os.system("git clone https://github.com/Und3rf10w/kali-anonsurf")
     os.system("cd kali-anonsurf")
-    os.system("sudo ./installer.sh")
+    os.system("sudo bash installer.sh")
 start = input("Gostaria de iniciar o kali anonsurf?(y/n): ")
 if start == ("y"):
     os.system("sudo anonsurf start")
